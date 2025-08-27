b17303f45acae2605f468f994f433f0d119663be
Palakdeep Singh <palakdeep8055@gmail.com>
2025-08-27 07:59:08 +0000

    ## Error Type Runtime FirebaseError
    ## Error Message
    Failed to get docu

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,7 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
-import { getFirestore, enableIndexedDbPersistence } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getFirestore, enableIndexedDbPersistence, initializeFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
